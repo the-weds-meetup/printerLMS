369bd1b38bfdfd0d99ea5a1e751908ccb41aa772
Brandan Teng <64852466+brandanteng@users.noreply.github.com>
2021-10-06 21:36:36 +0800

    feat: add submit new course screen

---
+++ webapp/src/views/SubmitCourse.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid">
+    <SubmitCourse />
+  </div>
+</template>
+
+<script>
+import SubmitCourse from '@/components/SubmitCourse.vue';
+
+export default {
+  name: 'SubmitCourse',
+  components: [SubmitCourse],
+};
+</script>
