bbab7b4343aa973cab26b2eaa558480961342d71
Jasmine Lim <jasmine-lim88@hotmail.com>
2021-10-08 21:11:04 +0800

    fixed formatting in main.js

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,11 +1,11 @@
 import '@babel/polyfill';
 import 'mutationobserver-shim';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
-import { createApp } from "vue";
+import { createApp } from 'vue';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).mount('#app');
