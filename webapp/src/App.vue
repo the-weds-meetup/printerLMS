3e6589511426fabd9fcf46ee0e927d904fceb326
Amos Tan <alphatrl1997@gmail.com>
2021-10-11 22:42:07 +0800

    refactor: add styles

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -2,4 +2,12 @@
   <router-view />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
