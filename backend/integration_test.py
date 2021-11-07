8d067684ecf371ecddaeed27fa0ca9cd8ee9d1d9
Amos Tan <alphatrl1997@gmail.com>
2021-11-07 23:18:23 +0800

    Revert "chore: TDD for TrainerController (#20)"
    This reverts commit ce6d4fd25c3f4f0dd3ec506c743f4f27d29026c3.

--- backend/integration_test.py
+++ backend/integration_test.py
@@ -15,8 +15,3 @@ suite.addTests(
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
-
-if result.wasSuccessful():
-    exit(0)
-else:
-    exit(1)
