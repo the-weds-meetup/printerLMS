d8dd439f70acc3d2e4c4a0b1262b68c47f3a4fdc
juleahn <64876278+juleahn@users.noreply.github.com>
2021-11-07 23:34:04 +0800

    added TestCoursePreqModule for TDD

---
+++ backend/tests/TestCoursePreqModule.py
@@ -0,0 +1,40 @@
+import unittest
+from unittest.mock import Mock, MagicMock, patch
+# from freezegun import freeze_time
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../controller"))
+)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../model")))
+
+
+from tests.TestApp import TestApp
+from model import CoursePreq
+from model import Course
+
+
+
+class TestCoursePreq(TestApp):
+
+    def test_get_course(self):
+        # course2 = Course.Course("How to Train Your Dragon", "Description here")
+        # course2.id = 1
+        # course2.
+        # course = CoursePreq.CoursePreq().get_course(1)
+        
+
+
+    def test_get_prereq_course(self):
+        pass
+
+def suite():
+    """This defines all the tests of a module"""
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestCoursePreq))
+    return suite
+
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite())
