4f053e2a8f7c700c705249267301f9951099ef7d
Amos Tan <alphatrl1997@gmail.com>
2021-11-07 23:18:23 +0800

    Revert "chore: TDD for TrainerController (#20)"
    This reverts commit ce6d4fd25c3f4f0dd3ec506c743f4f27d29026c3.

--- backend/tests/TestTrainerController.py
+++ backend/tests/TestTrainerController.py
@@ -1,9 +1,5 @@
-"""
-TestTrainerController.py
-Authored by: Amos Tan
-"""
-
 import unittest
+from unittest.mock import Mock, MagicMock, patch
 from freezegun import freeze_time
 import os, sys
 
@@ -19,8 +15,6 @@ from controller import TrainerController
 
 
 class TestTrainerController(TestApp):
-
-    # Test if classes are returned as expected
     def test_get_trainer_all_classes(self):
         self.assertEqual(
             len(TrainerController.TrainerController().get_all_classes(2)),
@@ -33,76 +27,6 @@ class TestTrainerController(TestApp):
             len(TrainerController.TrainerController().get_current_classes(2)),
             2,
         )
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_future_classes(self):
-        self.assertEqual(
-            len(TrainerController.TrainerController().get_future_classes(2)),
-            2,
-        )
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_past_classes(self):
-        self.assertEqual(
-            len(TrainerController.TrainerController().get_past_classes(2)),
-            2,
-        )
-
-    # Test if classes taught by Trainer is empty
-    @freeze_time("2021-10-22")
-    def test_get_trainer_all_classes_empty(self):
-        self.assertEqual(
-            len(TrainerController.TrainerController().get_all_classes(3)),
-            0,
-        )
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_current_classes_empty(self):
-        self.assertEqual(
-            len(TrainerController.TrainerController().get_current_classes(3)),
-            0,
-        )
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_future_classes_empty(self):
-        self.assertEqual(
-            len(TrainerController.TrainerController().get_future_classes(3)),
-            0,
-        )
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_past_classes_empty(self):
-        self.assertEqual(
-            len(TrainerController.TrainerController().get_past_classes(3)),
-            0,
-        )
-
-    # Test Exceptions
-    def test_get_trainer_get_all_classes_exception(self):
-        with self.assertRaises(Exception):
-            TrainerController.TrainerController().get_all_classes("string")
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_get_current_classes_exception(self):
-        with self.assertRaises(Exception):
-            TrainerController.TrainerController().get_current_classes("string")
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_future_classes_exception(self):
-        with self.assertRaises(Exception):
-            TrainerController.TrainerController().get_future_classes("string")
-
-    @freeze_time("2021-10-22")
-    def test_get_trainer_past_classes_exception(self):
-        with self.assertRaises(Exception):
-            TrainerController.TrainerController().get_future_classes("string")
-
-    # Test if learners are trainers
-    def test_is_trainer_true(self):
-        self.assertTrue(TrainerController.TrainerController().is_trainer(2))
-
-    def test_is_trainer_false(self):
-        self.assertFalse(TrainerController.TrainerController().is_trainer(3))
 
 
 if __name__ == "__main__":
