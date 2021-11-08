f8167d970c728e499aec41db136712d2daafa482
CHUNG KENG YAO _ <kychung.2019@smu.edu.sg>
2021-11-08 10:13:55 +0800

    Reformat

--- backend/tests/TestClassController.py
+++ backend/tests/TestClassController.py
@@ -19,16 +19,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../mode
 from tests.TestApp import TestApp
 from controller import ClassController
 
+
 class TestClassController(TestApp):
 
-    # Currently, database (TestApp.py) have 7 classes but all classes's enrolment_end_date are before current date so there is 0 enrollable class now. 
+    # Currently, database (TestApp.py) have 7 classes but all classes's enrolment_end_date are before current date so there is 0 enrollable class now.
     # If we change any of the classes's enrolement_end_date to any date after current date, it will appear as enrollable class.
     def test_get_all_enrollable_classes(self):
-        enrollable_classes = ClassController.ClassController().get_all_enrollable_classes()
-        self.assertEqual(len(enrollable_classes),0)
+        enrollable_classes = (
+            ClassController.ClassController().get_all_enrollable_classes()
+        )
+        self.assertEqual(len(enrollable_classes), 0)
 
     # Pass test case 1
-    # Get class by id = 1. 
+    # Get class by id = 1
     def test_get_class_pass1(self):
         classes1 = ClassController.ClassController().get_class(1)
         self.assertEqual(
@@ -38,18 +41,18 @@ class TestClassController(TestApp):
                 "course_id": 1,
                 "class_id": 1,
                 "max_capacity": 20,
-                "class_start_date":  '2021-10-05T16:00:00.000Z',
-                "class_end_date":  '2021-10-12T16:00:00.000Z',
-                "enrolment_start_date": '2021-10-04T00:00:00.000Z',
-                "enrolment_end_date": '2021-10-05T04:00:00.000Z',
-                "learners" : [],
-                "current_capacity" : 0,
-                "course_name" : 'Introduction to Programming',
+                "class_start_date": "2021-10-05T16:00:00.000Z",
+                "class_end_date": "2021-10-12T16:00:00.000Z",
+                "enrolment_start_date": "2021-10-04T00:00:00.000Z",
+                "enrolment_end_date": "2021-10-05T04:00:00.000Z",
+                "learners": [],
+                "current_capacity": 0,
+                "course_name": "Introduction to Programming",
             },
         )
-    
+
     # Pass test case 2
-    # Get class by id = 2. 
+    # Get class by id = 2
     def test_get_class_pass2(self):
         classes2 = ClassController.ClassController().get_class(2)
         self.assertEqual(
@@ -59,13 +62,13 @@ class TestClassController(TestApp):
                 "course_id": 2,
                 "class_id": 1,
                 "max_capacity": 20,
-                "class_start_date":  '2021-10-05T16:00:00.000Z',
-                "class_end_date":  '2021-10-12T16:00:00.000Z',
-                "enrolment_start_date": '2021-10-04T00:00:00.000Z',
-                "enrolment_end_date": '2021-10-05T04:00:00.000Z',
-                "learners" : [],
-                "current_capacity" : 0,
-                "course_name" : 'Information Systems and Innovation',
+                "class_start_date": "2021-10-05T16:00:00.000Z",
+                "class_end_date": "2021-10-12T16:00:00.000Z",
+                "enrolment_start_date": "2021-10-04T00:00:00.000Z",
+                "enrolment_end_date": "2021-10-05T04:00:00.000Z",
+                "learners": [],
+                "current_capacity": 0,
+                "course_name": "Information Systems and Innovation",
             },
         )
 
@@ -79,24 +82,25 @@ class TestClassController(TestApp):
     def test_get_class_fail2(self):
         self.assertRaises(Exception, ClassController.ClassController().get_class, 10)
 
-    # Get number of learners in class_id: 3 which will return 1. 
-    # There is only one learner enrolled into class_id: 3 so it should return 1. 
+    # Get number of learners in class_id: 3 which will return 1.
+    # There is only one learner enrolled into class_id: 3 so it should return 1.
     def test_get_class_learners1(self):
         get_learners1 = ClassController.ClassController().get_class_learners(3)
         self.assertEqual(len(get_learners1), 1)
-    
-    # Get number of learners in class_id: 4 which will return 1. 
-    # Even though there are two learners enrolled into class_id: 4 , one is_approved = True while another one is_approved = False. 
+
+    # Get number of learners in class_id: 4 which will return 1.
+    # Even though there are two learners enrolled into class_id: 4 , one is_approved = True while another one is_approved = False.
     # This means that there will only be one learner in class_id: 4
     def test_get_class_learners2(self):
         get_learners2 = ClassController.ClassController().get_class_learners(4)
         self.assertEqual(len(get_learners2), 1)
-            
+
     # Get a list of learners who are eligible for the course,but have yet to be enrolled or is not current taking any of the class right now.
     def test_get_class_non_enrolled_learners(self):
-        get_non_enrolled_learners = ClassController.ClassController().get_class_non_enrolled_learners(7)
+        get_non_enrolled_learners = (
+            ClassController.ClassController().get_class_non_enrolled_learners(7)
+        )
         self.assertEqual(len(get_non_enrolled_learners), 1)
-
 
 
 if __name__ == "__main__":
