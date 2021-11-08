29c4be9d311d86b1178ab62336dda9d832bebcde
juleahn <64876278+juleahn@users.noreply.github.com>
2021-11-08 09:50:06 +0800

    Completed TDD for CoursePreq Module

--- backend/integration_test.py
+++ backend/integration_test.py
@@ -6,31 +6,18 @@ loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 suite.addTests(
-    loader.loadTestsFromTestCase(TestTrainerController.TestTrainerController)
+    [
+        loader.loadTestsFromTestCase(TestCoursePreqModel.TestCoursePreq),
+        loader.loadTestsFromTestCase(TestTrainerController.TestTrainerController),
+    ]
 )
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-# if __name__ == "__main__":
-
-
-# suites = unittest.TestSuite(
-#     [
-#         TestTrainerController.suite(),
-#     ]
-# )
-# suites.run()
-=======
->>>>>>> 9c82e49aa9812ad82518bee6c2adca9257a0932a
-=======
 
 if result.wasSuccessful():
     exit(0)
 else:
     exit(1)
->>>>>>> ce6d4fd25c3f4f0dd3ec506c743f4f27d29026c3
--- backend/tests/TestCoursePreqModule.py
+++ backend/tests/TestCoursePreqModule.py
@@ -1,6 +1,12 @@
+"""
+TestCoursePreqModule.py
+Done by: Djulian Naval
+Email ID: dbnaval.2019
+"""
+
+
 import unittest
-from unittest.mock import Mock, MagicMock, patch
-# from freezegun import freeze_time
+
 import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -15,22 +21,45 @@ from model import CoursePreq
 from model import Course
 
 
-
 class TestCoursePreq(TestApp):
-
     def test_get_course(self):
-        # course2 = Course.Course("How to Train Your Dragon", "Description here")
-        # course2.id = 1
-        # course2.
-        # course = CoursePreq.CoursePreq().get_course(1)
-        pass
+        # get the only prereq couse in sqlite
+        precourse: CoursePreq = CoursePreq.CoursePreq.query.filter_by(
+            course_id=3
+        ).first()
+        course_compare = Course.Course(
+            "Web Application Development I",
+            "In this course, students be equipped with the knowledge and skill to develop a database-driven web application. PHP will be used as the vehicle of exploration. Other related topics like HTTP, HTML, CSS, Javascript will be covered as well. Students will learn the concepts gradually in the 13 weeks of the semester while building a web application of medium complexity.",
+        )
+        course_compare.id = 3
+        self.assertEqual(precourse.get_course().__repr__(), course_compare.__repr__())
 
-def suite():
-    """This defines all the tests of a module"""
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestCoursePreq))
-    return suite
+    def test_get_prereq_course(self):
+        # get the only prereq couse in sqlite
+        precourse: CoursePreq = CoursePreq.CoursePreq.query.filter_by(
+            course_id=3
+        ).first()
+        course_compare = Course.Course(
+            "Introduction to Programming",
+            "This course is intended for any student who wishes to gain some programming fundamentals, also known as the building blocks of Information Systems. The course introduces students to fundamental programming concepts and constructs, explains the process of developing a basic software application, and explains the basic concepts of object orientation. The student will experience the implementation of a basic software application. Python, a widely-used, high-level, general-purpose and interactive programming language, is used as the vehicle of exploration in this course.",
+        )
+        course_compare.id = 1
+        self.assertEqual(
+            precourse.get_prereq_course().__repr__(), course_compare.__repr__()
+        )
+
+    def test_serialise(self):
+        precourse: CoursePreq = CoursePreq.CoursePreq.query.filter_by(
+            course_id=3
+        ).first()
+        precourse_expected = CoursePreq.CoursePreq(3, 1)
+        precourse_expected.id = 1
+        self.assertEqual(precourse.serialise(), precourse_expected.serialise())
+
+    def test_empty_preqcourse_serialise(self):
+        with self.assertRaises(Exception):
+            CoursePreq.CoursePreq().serialise()
 
 
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(suite())
+    unittest.main()
